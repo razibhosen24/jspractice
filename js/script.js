bad02946ff3d4ec760e59511fdd2b74f3a4e90e9
razibhosen24 <90138277+razibhosen24@users.noreply.github.com>
2023-06-02 15:17:15 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+ 
+
+
+
+ // besic
+
+
+function textChange(){
+	document.getElementById('txtChng').innerHTML="Javascript is a lite weight, functional and object oriented scripting language.";
+}
+
+function play(){
+	document.getElementById('bird').src="images/play.gif";
+}
+
+function stop(){
+	document.getElementById('bird').src="images/stop.jpg";
+}
+
+function styleChange(){
+	document.getElementById('styleChng').style.fontSize="40px";
+}
+
+function windowAlert(){
+	window.alert("welcome");
+}
